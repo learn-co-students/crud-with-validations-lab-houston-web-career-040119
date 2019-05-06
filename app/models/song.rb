edf3aa7274113806d256187c999a55af09178bce
EC2 Default User <ec2-user@ip-172-31-33-230.us-east-2.compute.internal>
2019-05-06 01:07:28 +0000

    done

---
+++ app/models/song.rb
@@ -0,0 +1,18 @@
+class Song < ApplicationRecord
+
+    
+    validates :title, presence: true, uniqueness: {
+        scope: [:release_year, :artist_name]
+    }
+    
+    validates :released, inclusion: {in: [true, false]}
+    
+    validates :artist_name, presence: true
+    
+    validates :release_year, presence: true, numericality: {less_than_or_equal_to: Time.now.year},if: :is_released?
+        
+    def is_released?
+        released
+    end
+    
+end
